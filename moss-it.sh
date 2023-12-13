6535c1bd05d2a8083581ef9f249aa7b5831b05b6
urnotmiaomiao <60200863+urnotmiaomiao@users.noreply.github.com>
2023-12-13 11:41:53 -0600

    Update moss-it.sh

--- moss-it.sh
+++ moss-it.sh
@@ -5,3 +5,7 @@ read pNum
 
 ./moss -l c -d Project_$pNum_*/AOS-P$pNum-*/*.c
 ./moss -l java -d Project$pNum/AOS-P$pNum-*/*.java
+
+# to test the files inside project folders... should be like this, but I forgot 0-0
+# ./moss -l c -d Project_$pNum_*/AOS-P$pNum-*/*/*.c
+# ./moss -l java -d Project$pNum/AOS-P$pNum-*/*/*.java
